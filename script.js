ea3497dee99ad7b657bfc4b73beb1a6eee27a1f9
Phalgunsb12 <phalgunbalikai61@gmail.com>
2025-08-07 10:22:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Thank you for contacting us! We'll get back to you soon.");
-});
+function convertTemp(){
+    let temp = parseFloat(document.getElementById("temperature").value);
+    let unit = document.getElementById("unit").value;
+    let result = 0;
+
+    if(isNaN(temp)){
+        document.getElementById("result").innerText="Please Enter Valid Number";
+        return;
+    }
+    if(unit==="CtoF"){
+        result=(temp*9/5)+32;
+        document.getElementById("result").innerText=`${temp}°C = ${result.toFixed(2)}°F`;
+    }else{
+        result=(temp-32)*5/9;
+        document.getElementById("result").innerText=`${temp}°F = ${result.toFixed(2)}°C`;
+    }
+}
